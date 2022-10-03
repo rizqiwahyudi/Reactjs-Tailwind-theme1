bac63d6c5f14fc7e8efe39f839adb64a7146d4ac
rizqiwahyudi <rizqiwahyudi57@gmail.com>
2022-10-03 15:57:48 +0700

    Add: add new card with grafik

--- src/components/admin/Content.js
+++ src/components/admin/Content.js
@@ -1,6 +1,43 @@
 import React from "react";
+import Chart from "react-apexcharts";
 
 const Content = () => {
+    const series = [
+        {
+            name: "profit",
+            data: [43, 53, 50, 57, 87, 100, 23, 122, 23, 45, 234]
+        }
+    ];
+
+    const options = {
+        chart: {
+            locales: [{
+                "name": "id",
+                "options": {
+                  "months": ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+                  "shortMonths": ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+                  "days": ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
+                  "shortDays": ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+                  "toolbar": {
+                      "exportToSVG": "Download SVG",
+                      "exportToPNG": "Download PNG",
+                      "menu": "Menu",
+                      "selection": "Selection",
+                      "selectionZoom": "Selection Zoom",
+                      "zoomIn": "Zoom In",
+                      "zoomOut": "Zoom Out",
+                      "pan": "Panning",
+                      "reset": "Reset Zoom"
+                  }
+                }
+            }],
+            defaultLocale: "id"
+        },
+        xaxis: {
+            categories: [2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020]
+        }
+    };
+
     return (
         <div>
             <div className="container mx-auto px-5 py-5">
@@ -28,7 +65,7 @@ const Content = () => {
                     </div>
                 </div> */}
 
-                <div className="grid lg:grid-cols-3 grid-rows-3 gap-4 mt-4">
+                <div className="grid lg:grid-cols-3 gap-4 mt-4">
                     <div className="flex flex-row">
                         <div className="card w-96 h-[180px] bg-[#5E35B1] shadow-xl">
                             <div className="card-body">
@@ -38,14 +75,14 @@ const Content = () => {
                                     <div className="after:h-[190px] after:w-[190px] after:rounded-full after:bg-[#4527A0] after:absolute
                                     after:left-[230px] after:top-[-100px]"></div>
                                     <div> 
-                                        <button className="btn btn-sm bg-[#4527A0] h-[40px] border-none absolute">
+                                        <button className="btn btn-sm bg-[#4527A0] h-[40px] border-none">
                                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                                             <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 8.25h19.5M2.25 9h19.5m-16.5 5.25h6m-6 2.25h3m-3.75 3h15a2.25 2.25 0 002.25-2.25V6.75A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25v10.5A2.25 2.25 0 004.5 19.5z" />
                                             </svg>
                                         </button>
                                     </div>
                                     <div className="">
-                                        <button className="btn btn-sm bg-[#5E35B1] float-right border-none">
+                                        <button className="btn btn-sm bg-[#5E35B1] float-right border-none absolute left-[200px]">
                                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
                                             <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM12.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM18.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
                                             </svg>
@@ -74,7 +111,7 @@ const Content = () => {
                                         </button>
                                     </div>
                                     <div className="">
-                                        <button className="btn btn-sm bg-[#5E35B1] float-right border-none z-40">
+                                        <button className="btn btn-sm bg-[#1E88E5] float-right border-none absolute left-[235px]">
                                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
                                             <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM12.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM18.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
                                             </svg>
@@ -91,15 +128,15 @@ const Content = () => {
                         <div className="card w-90 h-[80px] bg-[#1E88E5] shadow-xl mb-4">
                             <div className="card-body">
                                 <div class="grid grid-cols-2">
-                                    <div className="after:h-[160px] after:w-[160px] after:rounded-full after:bg-[#1976D3] after:absolute
+                                    {/* <div className="after:h-[160px] after:w-[160px] after:rounded-full after:bg-[#1976D3] after:absolute
                                     after:left-[220px] after:top-[-70px]"></div>
                                     <div className="after:h-[190px] after:w-[190px] after:rounded-full after:bg-[#1565C0] after:absolute
-                                    after:left-[280px] after:top-[-70px]"></div>
-                                    <div> 
+                                    after:left-[280px] after:top-[-70px]"></div> */}
+                                    <div className=""> 
                                         <button className="btn btn-sm bg-[#1565C0] h-[40px] border-none">
-                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                            <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 8.25h19.5M2.25 9h19.5m-16.5 5.25h6m-6 2.25h3m-3.75 3h15a2.25 2.25 0 002.25-2.25V6.75A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25v10.5A2.25 2.25 0 004.5 19.5z" />
-                                            </svg>
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 15.75V18m-7.5-6.75h.008v.008H8.25v-.008zm0 2.25h.008v.008H8.25V13.5zm0 2.25h.008v.008H8.25v-.008zm0 2.25h.008v.008H8.25V18zm2.498-6.75h.007v.008h-.007v-.008zm0 2.25h.007v.008h-.007V13.5zm0 2.25h.007v.008h-.007v-.008zm0 2.25h.007v.008h-.007V18zm2.504-6.75h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V13.5zm0 2.25h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V18zm2.498-6.75h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V13.5zM8.25 6h7.5v2.25h-7.5V6zM12 2.25c-1.892 0-3.758.11-5.593.322C5.307 2.7 4.5 3.65 4.5 4.757V19.5a2.25 2.25 0 002.25 2.25h10.5a2.25 2.25 0 002.25-2.25V4.757c0-1.108-.806-2.057-1.907-2.185A48.507 48.507 0 0012 2.25z" />
+                                        </svg>
                                         </button>
                                     </div>
                                     <div className="">
@@ -133,6 +170,29 @@ const Content = () => {
                             </div>
                         </div>
                     </div>
+                </div>
+
+                <div className="grid lg:grid-cols-3 gap-4 mt-4">
+                        <div className="card w-[640px] h-auto bg-white shadow-xl">
+                            <div className="card-body">
+                                <div class="grid grid-cols-2 relative">
+                                    <div> 
+                                        <p className="text-slate-400 text-sm mt-[-10px]">Total Growth</p>
+                                        <h2 className="font-money text-[20px] font-bold text-black">$500.00</h2>
+                                    </div>
+                                    <div className="">
+                                        <select name="" id="" className="w-30 border focus:ring-2 float-right">
+                                            <option value="">Today</option>
+                                            <option value="">This Month</option>
+                                            <option value="">This Year</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div className="mx-auto">
+                                    <Chart options={options} series={series} type="bar" />
+                                </div>
+                            </div>
+                        </div>
                 </div>
             </div>
 
