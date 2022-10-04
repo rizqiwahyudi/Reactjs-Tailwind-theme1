5526554d7c3f44f4cedf6f1a5ea54e2e9b765486
rizqiwahyudi <rizqiwahyudi57@gmail.com>
2022-10-04 14:09:03 +0700

    Fix: Fix card

--- src/components/admin/Content.js
+++ src/components/admin/Content.js
@@ -31,7 +31,7 @@ const Content = () => {
                   }
                 }
             }],
-            defaultLocale: "id"
+            defaultLocale: "id",
         },
         xaxis: {
             categories: [2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020]
@@ -40,7 +40,7 @@ const Content = () => {
 
     return (
         <div>
-            <div className="container mx-auto px-5 py-5">
+            <div className="mx-auto px-5 py-5">
                 {/* <div className="grid grid-cols-3 gap-4">
                     <div className="flex flex-row">
                         <div className="card w-80 h-44 glass bg-[#5e35b1] shadow-md transition transform hover:bg-[#5e35b1] hover:-translate-y-1 motion-reduce:transition-none motion-reduce:hover:transform-none">
@@ -128,71 +128,71 @@ const Content = () => {
                         <div className="card w-90 h-[80px] bg-[#1E88E5] shadow-xl mb-4">
                             <div className="card-body">
                                 <div class="grid grid-cols-2">
-                                    {/* <div className="after:h-[160px] after:w-[160px] after:rounded-full after:bg-[#1976D3] after:absolute
-                                    after:left-[220px] after:top-[-70px]"></div>
-                                    <div className="after:h-[190px] after:w-[190px] after:rounded-full after:bg-[#1565C0] after:absolute
-                                    after:left-[280px] after:top-[-70px]"></div> */}
+                                    <div className="after:h-[160px] after:w-[160px] after:rounded-full after:bg-[#3394E9] after:absolute
+                                    after:left-[230px] after:top-[-120px]"></div>
+                                    <div className="after:h-[190px] after:w-[190px] after:rounded-full after:bg-[#3394E9] after:absolute
+                                    after:left-[280px] after:top-[-30px] after:opacity-70"></div>
                                     <div className=""> 
-                                        <button className="btn btn-sm bg-[#1565C0] h-[40px] border-none">
+                                        <button className="btn btn-sm bg-[#1565C0] border-none">
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                                             <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 15.75V18m-7.5-6.75h.008v.008H8.25v-.008zm0 2.25h.008v.008H8.25V13.5zm0 2.25h.008v.008H8.25v-.008zm0 2.25h.008v.008H8.25V18zm2.498-6.75h.007v.008h-.007v-.008zm0 2.25h.007v.008h-.007V13.5zm0 2.25h.007v.008h-.007v-.008zm0 2.25h.007v.008h-.007V18zm2.504-6.75h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V13.5zm0 2.25h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V18zm2.498-6.75h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V13.5zM8.25 6h7.5v2.25h-7.5V6zM12 2.25c-1.892 0-3.758.11-5.593.322C5.307 2.7 4.5 3.65 4.5 4.757V19.5a2.25 2.25 0 002.25 2.25h10.5a2.25 2.25 0 002.25-2.25V4.757c0-1.108-.806-2.057-1.907-2.185A48.507 48.507 0 0012 2.25z" />
                                         </svg>
                                         </button>
                                     </div>
-                                    <div className="">
-                                        <button className="btn btn-sm bg-[#5E35B1] float-right border-none z-40">
-                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
-                                            <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM12.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM18.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
-                                            </svg>
-                                        </button>
+                                    <div className="flex-col">
+                                        <h2 className="text-white font-bold">$203k</h2>
+                                        <h4 className="text-white">Total Income</h4>
                                     </div>
                                 </div>
-                                <h2 className="font-money text-[38px] font-bold text-white">$500.00</h2>
-                                <h4 className="text-slate-400 font-medium text-[20px] mt-[-10px]">Total Earning</h4>
                             </div>
                         </div>
 
-                        <div className="card w-90 h-[80px] bg-[#c2df41] shadow-xl">
-                            <div className="grid grid-cols-4">
-                                <div className="h-20 content-center"> 
-                                    <div className="after:h-[160px] after:w-[160px] after:rounded-full after:bg-[#1976D3] after:absolute
-                                    after:left-[220px] after:top-[-70px]"></div>
-                                    <div className="after:h-[190px] after:w-[190px] after:rounded-full after:bg-[#1565C0] after:absolute
-                                    after:left-[280px] after:top-[-70px]"></div>
-                                    <button className="top-[50px] btn btn-sm bg-[#1565C0] h-[40px] border-none">
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 8.25h19.5M2.25 9h19.5m-16.5 5.25h6m-6 2.25h3m-3.75 3h15a2.25 2.25 0 002.25-2.25V6.75A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25v10.5A2.25 2.25 0 004.5 19.5z" />
-                                        </svg>
-                                    </button>
+                        <div className="card w-90 h-[80px] bg-white shadow-xl">
+                            <div className="card-body">
+                                <div className="grid grid-cols-2">
+                                    <div className="h-20 content-center"> 
+                                        <div className="after:h-[160px] after:w-[160px] after:rounded-full after:bg-gradient-to-r after:from-[#FFF1C6] after:absolute
+                                        after:left-[220px] after:top-[-120px]"></div>
+                                        <div className="after:h-[190px] after:w-[190px] after:rounded-full after:bg-gradient-to-br after:from-[#FFF1C6] after:absolute
+                                        after:left-[280px] after:top-[-30px] after:opacity-70"></div>
+                                        <button className="btn btn-sm bg-[#FFF8E1] border-none">
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-[#FFC107]">
+                                                <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 21v-7.5a.75.75 0 01.75-.75h3a.75.75 0 01.75.75V21m-4.5 0H2.36m11.14 0H18m0 0h3.64m-1.39 0V9.349m-16.5 11.65V9.35m0 0a3.001 3.001 0 003.75-.615A2.993 2.993 0 009.75 9.75c.896 0 1.7-.393 2.25-1.016a2.993 2.993 0 002.25 1.016c.896 0 1.7-.393 2.25-1.016a3.001 3.001 0 003.75.614m-16.5 0a3.004 3.004 0 01-.621-4.72L4.318 3.44A1.5 1.5 0 015.378 3h13.243a1.5 1.5 0 011.06.44l1.19 1.189a3 3 0 01-.621 4.72m-13.5 8.65h3.75a.75.75 0 00.75-.75V13.5a.75.75 0 00-.75-.75H6.75a.75.75 0 00-.75.75v3.75c0 .415.336.75.75.75z" />
+                                            </svg>
+                                        </button>
+                                    </div>
+
+                                    <div className="flex-col">
+                                        <h2 className="text-black font-bold">$203k</h2>
+                                        <h4 className="text-black">Total Income</h4>
+                                    </div>
                                 </div>
-                                <div className="col-span-3 h-20 content-center"><h4 className="text-slate-400 font-medium text-[20px] mt-[-10px]">Total Earning</h4></div>
-                               
                             </div>
                         </div>
                     </div>
                 </div>
 
                 <div className="grid lg:grid-cols-3 gap-4 mt-4">
-                        <div className="card w-[640px] h-auto bg-white shadow-xl">
-                            <div className="card-body">
-                                <div class="grid grid-cols-2 relative">
-                                    <div> 
-                                        <p className="text-slate-400 text-sm mt-[-10px]">Total Growth</p>
-                                        <h2 className="font-money text-[20px] font-bold text-black">$500.00</h2>
-                                    </div>
-                                    <div className="">
-                                        <select name="" id="" className="w-30 border focus:ring-2 float-right">
-                                            <option value="">Today</option>
-                                            <option value="">This Month</option>
-                                            <option value="">This Year</option>
-                                        </select>
-                                    </div>
+                    <div className="card w-[630px] bg-white shadow-xl">
+                        <div className="card-body">
+                            <div class="grid grid-cols-2 relative">
+                                <div> 
+                                    <p className="text-slate-400 text-sm mt-[-10px]">Total Growth</p>
+                                    <h2 className="font-money text-[20px] font-bold text-black">$500.00</h2>
                                 </div>
-                                <div className="mx-auto">
-                                    <Chart options={options} series={series} type="bar" />
+                                <div className="">
+                                    <select name="" id="" className="w-30 border focus:ring-2 float-right">
+                                        <option value="">Today</option>
+                                        <option value="">This Month</option>
+                                        <option value="">This Year</option>
+                                    </select>
                                 </div>
                             </div>
+                            <div className="mx-auto">
+                                <Chart options={options} series={series} type="bar"/>
+                            </div>
                         </div>
+                    </div>
                 </div>
             </div>
 
--- src/components/admin/Navbar.js
+++ src/components/admin/Navbar.js
@@ -28,7 +28,7 @@ export default function Navbar() {
         <div className="navbar-center grow">
           <div className="form-control ml-4 py-2">
             <div className="input-group lg:flex hidden">
-              <input type="text" placeholder="Search…" className="w-[300px] input input-bordered bg-white placeholder:contrast-less" />
+              <input type="text" placeholder="Search…" className="w-[300px] input input-bordered bg-white placeholder:contrast-less focus:ring-2 focus:ring-sky-400" />
               <button className="btn btn-square bg-[#EDE7F6] hover:bg-[#5e35b1]">
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
               </button>
@@ -39,18 +39,18 @@ export default function Navbar() {
           <button className="btn btn-ghost btn-square rounded-lg bg-[#EDE7F6] hover:bg-[#5e35b1] group">
             <div className="indicator">
               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" /></svg>
-              <span className="badge badge-xs badge-primary indicator-item group-hover:bg-purple-500 group-hover:opacity-70"></span>
+              <span className="badge badge-xs badge-primary indicator-item group-hover:bg-violet-300"></span>
             </div>
           </button>
 
           <div className="dropdown">
-            <label tabIndex={0} className="btn hover:bg-[#2196f3] btn-ghost m-1 gap-3 rounded-3xl bg-sky-100">
+            <label tabIndex={0} className="btn hover:bg-[#2196f3] btn-ghost m-1 gap-3 rounded-3xl bg-sky-100 group">
               <div className="avatar placeholder">
                 <div className="bg-neutral-focus text-neutral-content rounded-full w-8">
                   <span className="text-xs">AA</span>
                 </div>
               </div>
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 stroke-cyan-500 hover:stroke-cyan-100">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 stroke-cyan-500 group-hover:stroke-cyan-100">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.324.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 011.37.49l1.296 2.247a1.125 1.125 0 01-.26 1.431l-1.003.827c-.293.24-.438.613-.431.992a6.759 6.759 0 010 .255c-.007.378.138.75.43.99l1.005.828c.424.35.534.954.26 1.43l-1.298 2.247a1.125 1.125 0 01-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.57 6.57 0 01-.22.128c-.331.183-.581.495-.644.869l-.213 1.28c-.09.543-.56.941-1.11.941h-2.594c-.55 0-1.02-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 01-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 01-1.369-.49l-1.297-2.247a1.125 1.125 0 01.26-1.431l1.004-.827c.292-.24.437-.613.43-.992a6.932 6.932 0 010-.255c.007-.378-.138-.75-.43-.99l-1.004-.828a1.125 1.125 0 01-.26-1.43l1.297-2.247a1.125 1.125 0 011.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.087.22-.128.332-.183.582-.495.644-.869l.214-1.281z" />
                 <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
               </svg>
@@ -82,12 +82,16 @@ export default function Navbar() {
         {/* Navigation */}
         <div className={"h-auto flex-none " + (showNavbar ? " w-[260px] translate-x-0 ease-in duration-500 lg:block hidden " : " w-[0px] translate-x-[-250px] ease-out duration-500 ")} >
             <ul className="menu p-5">
+            <p className="text-black ml-2 font-semibold text-sm">Dashboard</p>
               <li className="hover:text-[#5e35b1] hover:bg-[#EDE7F6] rounded-xl">
                 <a href='dashboard' >
                   <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" /></svg>
-                Item 2
+                Dashboard
                 </a>
               </li>
+              <hr className="my-4"></hr>
+              <p className="text-black ml-2 font-semibold text-sm">Pages</p>
+              <span className="text-xs text-slate-400 ml-2 font-semibold mb-4 mt-1">Pages Caption</span>
               <li className="hover:text-[#5e35b1] hover:bg-[#EDE7F6] rounded-xl">
                 <a>
                   <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
@@ -104,7 +108,7 @@ export default function Navbar() {
         </div>
 
         {/* Content */}
-        <div className="grow bg-[#e3f2fd] rounded-tl-xl sm:grid-cols-1">
+        <div className="container grow bg-[#e3f2fd] rounded-tl-xl sm:grid-cols-1 overflow-auto">
           <Content/>
         </div>
       </div>
