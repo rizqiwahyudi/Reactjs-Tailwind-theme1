4e7f0e3134b615791aab406154766daa1438a2e3
rizqiwahyudi <rizqiwahyudi57@gmail.com>
2022-10-05 15:03:03 +0700

    change navbar

--- src/components/admin/Navbar.js
+++ src/components/admin/Navbar.js
@@ -82,7 +82,7 @@ export default function Navbar() {
         {/* Navigation */}
         <div className={"h-auto flex-none " + (showNavbar ? " w-[260px] translate-x-0 ease-in duration-500 lg:block hidden " : " w-[0px] translate-x-[-250px] ease-out duration-500 ")} >
             <ul className="menu p-5">
-            <p className="text-black ml-2 font-semibold text-sm">Dashboard</p>
+            <p className="text-black ml-2 font-semibold text-sm mb-2">Dashboard</p>
               <li className="hover:text-[#5e35b1] hover:bg-[#EDE7F6] rounded-xl">
                 <a href='dashboard' >
                   <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" /></svg>
@@ -99,7 +99,7 @@ export default function Navbar() {
                 </a>
               </li>
               <hr className="my-4"></hr>
-              <p className="text-black ml-2 font-semibold text-sm">Utilities</p>
+              <p className="text-black ml-2 font-semibold text-sm mb-2">Utilities</p>
               <li className="hover:text-[#5e35b1] hover:bg-[#EDE7F6] rounded-xl">
                 <a>
                   <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" /></svg>
