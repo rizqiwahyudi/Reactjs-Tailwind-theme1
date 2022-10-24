2aed762f2d7859eeb646d56d598bb5ae0e2a6a5f
rizqiwahyudi <rizqiwahyudi57@gmail.com>
2022-10-24 15:54:20 +0700

    change landing page and navbar

--- src/components/admin/Navbar.js
+++ src/components/admin/Navbar.js
@@ -21,8 +21,10 @@ export default function Navbar(props) {
             {/* Mobile Main Menu */}
             <ul tabIndex={0} className={"menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52" + (showNavbar ? " hidden" : " lg:hidden block")}>
               <li><a href="/">Homepage</a></li>
-              <li><a href="/register">Register</a></li>
-              <li><a href="/login">Login</a></li>
+              <li><a href="/crud">Sample CRUD Table</a></li>
+              <li><a href="/register" target="_blank">Register</a></li>
+              <li><a href="/login" target="_blank">Login</a></li>
+              <li><a href="/landing-page" target="_blank">Landing Page</a></li>
             </ul>
           </div>
         </div>
@@ -121,7 +123,7 @@ export default function Navbar(props) {
                 </a>
               </li>
               <li className="hover:text-[#5e35b1] hover:bg-[#EDE7F6] rounded-xl">
-                <a href="/landing-page">
+                <a href="/landing-page" target="_blank">
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M12 21a9.004 9.004 0 008.716-6.747M12 21a9.004 9.004 0 01-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 017.843 4.582M12 3a8.997 8.997 0 00-7.843 4.582m15.686 0A11.953 11.953 0 0112 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0121 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0112 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 013 12c0-1.605.42-3.113 1.157-4.418" />
                   </svg>
