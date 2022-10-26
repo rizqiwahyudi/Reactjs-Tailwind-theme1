3bb94009f5375c867b5662e8f79775c9ea4f47a9
rizqiwahyudi <rizqiwahyudi57@gmail.com>
2022-10-26 16:57:59 +0700

    change landing page

--- src/components/landingPage/LandingPage.js
+++ src/components/landingPage/LandingPage.js
@@ -133,7 +133,7 @@ const LandingPage = () => {
                         <div className="w-full md:w-1/2 p-8">
                             <div className="inline-block mb-6 px-2 py-1 font-semibold bg-green-100 rounded-full">
                             <div className="flex flex-wrap items-center -m-1">
-                                <div className="w-auto p-1"><a className="text-sm" href="">👋 We are hiring! View open roles</a></div>
+                                <div className="w-auto p-1"><a className="text-sm text-black" href="">👋 We are hiring! View open roles</a></div>
                                 <div className="w-auto p-1">
                                 <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M8.66667 3.41675L12.75 7.50008M12.75 7.50008L8.66667 11.5834M12.75 7.50008L2.25 7.50008" stroke="black" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path>
